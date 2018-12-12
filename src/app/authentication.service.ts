00c034595316356b1194b0304a05f3cde4ee1496
Ambati Raghu <Ambati.Raghu@karvy.com>
2018-12-12 15:18:46 +0530

    initial commit

---
+++ src/app/authentication.service.ts
@@ -0,0 +1,66 @@
+import {Injectable} from '@angular/core';
+// import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {JwksValidationHandler, OAuthService} from 'angular-oauth2-oidc';
+import {authConfig} from './auth.config';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(private oauthService: OAuthService) {
+    console.log('AUTH CONSTRUCTOR');
+    this.configureOauthApi();
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.tryLogin({});
+  }
+
+  private configureOauthApi() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.setupAutomaticSilentRefresh();
+  }
+
+  getAccessToken(): string {
+    return this.oauthService.getAccessToken();
+  }
+
+  loadUserDetails() {
+    this.oauthService.loadUserProfile();
+  }
+
+  get access_token_expiration() {
+    return this.oauthService.getAccessTokenExpiration();
+  }
+
+  get fullName() {
+    return this.getClaimEntry('full_name');
+  }
+
+  firstName() {
+    return this.getClaimEntry('first_name');
+  }
+
+  isLoggedIn() {
+    if (this.oauthService.getAccessToken() === null) {
+      return false;
+    }
+    return true;
+  }
+
+  public login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  public logout() {
+    this.oauthService.logOut();
+  }
+
+  private getClaimEntry(entry) {
+    let claims = this.oauthService.getIdentityClaims();
+
+    if (!claims) {
+      this.oauthService.loadUserProfile();
+    } else {
+      return claims[entry];
+    }
+  }
+
+}
